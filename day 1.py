f9b110b40b384599bccab871c87d66d29d65440f
Nikhil Sharma <panditnikhil897@gmail.com>
2026-04-13 16:54:19 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,112 @@
+
+# print("hello world")
+# print("My name is nikhil")
+# print(23)   
+
+## variable
+# name="nikhil"
+# age=20
+# print("my name is :",name   ) 
+# print(type(age))  
+
+# old=False
+# z=None
+# print(type(old))
+#  sum
+# a=2
+# b=6
+# sum=a+b
+# print(sum)
+
+# a=2
+# b=2.32
+# print(a+b)
+# name=input("enter the name")
+# print("welcome", name)
+
+ #practice
+ 
+# 
+# a=float(input("enter num1:"))
+# b=float(input("enter num2:"))
+# print((a+b)/2)
+
+# a=int(input("enter the number1:"))
+# b=int(input("enter the number2:"))
+# print(a>=b)
+ 
+# str="I am studing python from ApnaCollege"
+# print(str.endswith("ge"))
+# print(str.capitalize())
+# print(str.replace("python","java"))
+# print(str.find("o"))
+# print(str.count("o"))
+
+# a=input("enter the name")
+# print(len(a))
+
+# str="Hi,$iam the $ symbol"
+# print(str.count("$"))
+
+# conditional statement
+
+# light="Green"
+# if(light=="Green"):
+#     print("go")
+# elif(light=="Red"):
+#     print("stop")
+# else:
+#     print("light is broken")
+    
+ 
+# num=5
+# if(num>2):
+#     print("greater than 2")
+# elif(num>3):
+#     print("greater than 3")
+
+# marks=int(input("enter the marks"))
+# if(marks>=90):
+#     grade="A"
+# elif(90> marks>=80):
+#     grade="B"
+# elif(marks>=70 and marks<80):
+#     grade="C"
+# else:
+#     grade="D"
+# print(grade)
+
+# Nesting
+# age=34
+# if(age>=18):
+#     if(age>=80):
+#         print("cannot drive")
+#     else:
+#         print("can drive")
+# else:
+#     print("cannot drive")
+
+#practice
+# a=int(input("enter the number"))
+# if(a%2==0):
+#     number="even"
+# else:
+#     number="odd"
+# print(number)
+
+# a=int(input("number1"))
+# b=int(input("number2"))
+# c=int(input("number3"))
+# if(a>b and a>c):
+#     print("a is greater")
+# elif(b>c):
+#     print(" b is greater")
+# else:
+#     print("c is greater")
+
+# 3
+a=int(input("enter the number"))
+if(a%7==0):
+    print("number is multiple of 7")
+else:
+    print("not multiple of 7")
